99debcd84966dfff318f82f5cac0879f0eb0f420
ABHISHEK MAURYA <abhishek.maurya@daffodilsw.com>
2024-04-07 08:41:06 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,7 @@
+console.log(__dirname);
+console.log(__filename);
+//console.log(process);
+console.log(require);
+// setInterval(()=>{
+//     console.log('hello world');
+// }, 1000)
---
+++ 3-modules.js
@@ -0,0 +1,13 @@
+//Modules Encapsulated COde (Only share minimum)
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade');
+
+//console.log(names);
+
+// sayHi('susan');
+
+// sayHi(names.john)
+
+// sayHi(names.peter)
